8ada81420eb2d2820df25b1ec9e3e75123ddcf65
Jonas Hahn <herzock@gmail.com>
2025-10-01 21:19:48 +0300

    Include private images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingIncludes: {
+    '/api/full/[id]': ['./src/private/images/**/*'],
+  },
 }
 
 export default nextConfig
